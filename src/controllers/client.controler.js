42b4d1cd1475c2637dad1c94172007ff07c25aeb
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-12 12:14:11 -0300

    feat: route update client

--- src/controllers/client.controler.js
+++ src/controllers/client.controler.js
@@ -37,10 +37,27 @@ export async function getclient(req,res){
         const clients = await db.query("SELECT * FROM customers WHERE id=$1;",[req.params.id])
 
         if(clients.rowCount == 0 ) return res.sendStatus(404)
-        
+
         res.send(clients.rows)
     } catch(err){
 
         res.status(500).send(err)
     }
+}
+
+export async function putClient(req,res){
+
+    try{
+        const data = await db.query("SELECT * FROM customers WHERE cpf=$1 AND id<>$2",[req.body.cpf,req.params.id])
+
+        if(data.rowCount != 0) return res.status(409).send("invalid CPF")
+
+        const resposta = await db.query(`UPDATE customers SET name=$1,phone=$2,cpf=$3, birthday=$4 
+        WHERE id=$5`,[req.body.name,req.body.phone,req.body.cpf,req.body.birthday,req.params.id])
+        res.sendStatus(200)
+
+    } catch(err){
+        res.status(500).send(err)
+    }
+
 }
--- src/routers/client.router.js
+++ src/routers/client.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import validateClient from "../middlewares/client.middleware.js";
-import { getclients, postClient, getclient } from "../controllers/client.controler.js";
+import { getclients, postClient, getclient, putClient } from "../controllers/client.controler.js";
 
 const route = Router()
 
@@ -10,5 +10,6 @@ route.get("/customers",getclients)
 
 route.get("/customers/:id",getclient)
 
+route.put("/customers/:id",validateClient,putClient)
 
 export default route
